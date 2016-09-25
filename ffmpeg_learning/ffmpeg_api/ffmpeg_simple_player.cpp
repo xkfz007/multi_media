49c919b83d1cf5d1fa636b9a33fa168d34be3c35
Felix <wintelx264@163.com>
2016-09-25 19:38:44 +0800

    add ffmpeg_learning

---
+++ ffmpeg_learning/ffmpeg_api/ffmpeg_simple_player.cpp
@@ -0,0 +1,167 @@
+//ffmpeg simple player
+//
+#include <stdio.h>
+//#include <tchar.h>
+//#include <io.h>
+//#include <direct.h>
+extern "C"
+{
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
+#include "libswscale/swscale.h"
+	//SDL
+#include "SDL2/SDL.h"
+#undef main
+};
+
+//Refresh Event
+#define SFM_REFRESH_EVENT  (SDL_USEREVENT + 1)
+
+int main(int argc, char* argv[])
+{
+	AVFormatContext	*pFormatCtx;
+	int				i, videoindex;
+	AVCodecContext	*pCodecCtx;
+	AVCodec			*pCodec;
+	AVFrame	*pFrame,*pFrameYUV;
+	uint8_t *out_buffer;
+	AVPacket *packet;
+	int ret, frameFinished;
+
+	//------------SDL----------------
+	int screen_w,screen_h;
+	SDL_Window *screen; 
+	SDL_Renderer* sdlRenderer;
+	SDL_Texture* sdlTexture;
+	SDL_Rect sdlRect;
+	SDL_Thread *video_tid;
+	SDL_Event event;
+	struct SwsContext *img_convert_ctx;
+	if(argc<2)
+	{
+		fprintf(stderr,"parameter is not enough\n");
+		return -1;
+	}
+	char *filepath=argv[1];
+	av_register_all();
+	avformat_network_init();
+	pFormatCtx = avformat_alloc_context();
+
+	if(avformat_open_input(&pFormatCtx,filepath,NULL,NULL)!=0){
+		printf("Couldn't open input stream.\n");
+		return -1;
+	}
+	if(avformat_find_stream_info(pFormatCtx,NULL)<0){
+		printf("Couldn't find stream information.\n");
+		return -1;
+	}
+	videoindex=-1;
+	for(i=0; i<pFormatCtx->nb_streams; i++) {
+		if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
+			videoindex=i;
+			break;
+		}
+	}
+	if(videoindex==-1){
+		printf("Didn't find a video stream.\n");
+		return -1;
+	}
+	pCodecCtx=pFormatCtx->streams[videoindex]->codec;
+	pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+	if(pCodec==NULL){
+		printf("Codec not found.\n");
+		return -1;
+	}
+	if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+		printf("Could not open codec.\n");
+		return -1;
+	}
+	pFrame=av_frame_alloc();
+	pFrameYUV=av_frame_alloc();
+	out_buffer=(uint8_t *)av_malloc(avpicture_get_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height));
+	avpicture_fill((AVPicture *)pFrameYUV, out_buffer, AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height);
+
+	//Output Info-----------------------------
+	printf("---------------- File Information ---------------\n");
+	av_dump_format(pFormatCtx,0,filepath,0);
+	printf("-------------------------------------------------\n");
+
+	img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 
+		pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL); 
+
+
+	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {  
+		printf( "Could not initialize SDL - %s\n", SDL_GetError()); 
+		return -1;
+	} 
+	//SDL 2.0 Support for multiple windows
+	screen_w = pCodecCtx->width;
+	screen_h = pCodecCtx->height;
+	screen = SDL_CreateWindow("Simplest ffmpeg player's Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		screen_w, screen_h,SDL_WINDOW_OPENGL);
+
+	if(!screen) {  
+		printf("SDL: could not create window - exiting:%s\n",SDL_GetError());  
+		return -1;
+	}
+	sdlRenderer = SDL_CreateRenderer(screen, -1, 0);  
+	//IYUV: Y + U + V  (3 planes)
+	//YV12: Y + V + U  (3 planes)
+	sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING,pCodecCtx->width,pCodecCtx->height);  
+
+	sdlRect.x=0;
+	sdlRect.y=0;
+	sdlRect.w=screen_w;
+	sdlRect.h=screen_h;
+
+	packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+
+	//video_tid = SDL_CreateThread(sfp_refresh_thread,NULL,NULL);
+	//------------SDL End------------
+	//Event Loop
+
+	while(av_read_frame(pFormatCtx,packet)>=0){
+		//Wait
+		if(packet->stream_index==videoindex){
+			ret = avcodec_decode_video2(pCodecCtx, pFrame, &frameFinished, packet);
+			if(ret < 0){
+				printf("Decode Error.\n");
+				return -1;
+			}
+			if(frameFinished){
+				sws_scale(img_convert_ctx, (const uint8_t* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
+				//SDL---------------------------
+				SDL_UpdateTexture( sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0] );  
+				SDL_RenderClear( sdlRenderer );  
+				//SDL_RenderCopy( sdlRenderer, sdlTexture, &sdlRect, &sdlRect );  
+				SDL_RenderCopy( sdlRenderer, sdlTexture, NULL, NULL);  
+				SDL_RenderPresent( sdlRenderer );  
+				//SDL End-----------------------
+			}
+		}
+		av_free_packet(packet);
+
+		SDL_PollEvent(&event);
+		switch(event.type) {
+		case SDL_QUIT:
+			SDL_Quit();
+			exit(0);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	sws_freeContext(img_convert_ctx);
+
+	SDL_Quit();
+	//--------------
+	av_frame_free(&pFrameYUV);
+	av_frame_free(&pFrame);
+	avcodec_close(pCodecCtx);
+	avformat_close_input(&pFormatCtx);
+
+	return 0;
+}
+
