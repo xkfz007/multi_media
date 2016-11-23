5ce1a1f512ad7001d603ce960dbffe4591608fd2
Felix <wintelx264@163.com>
2016-11-23 20:12:48 +0800

    add compiling script

--- x265_latest/build/config.sh
+++ x265_latest/build/config.sh
@@ -1,4 +1,14 @@
-    X265_FILE="x265_2.0.tar.gz"
-    X265_CMD='cmake -G "Unix Makefiles" "`pwd`/source" -DCMAKE_INSTALL_PREFIX="`pwd`" -DENABLE_STATIC:bool=on -DENABLE_SHARED:bool=off -DENABLE_CLI:bool=off'
-    #X265_PC='x265.pc'
-    compile_lib "$X265_FILE" "$X265_CMD" #"$X265_PC"
+#!/bin/bash
+current_dir=`pwd`
+build_dir=$current_dir/builds
+if [ ! -e $build_dir ];then
+    mkdir -p $build_dir
+fi
+source_dir=`readlink -f $current_dir/../source`
+echo $source_dir
+CMD="cmake -G \"Unix Makefiles\" $source_dir -DCMAKE_INSTALL_PREFIX=$build_dir -DENABLE_STATIC:bool=on -DENABLE_SHARED:bool=off -DENABLE_CLI:bool=off"
+cd $build_dir
+echo $CMD
+eval $CMD
+make && make install
+cd -
